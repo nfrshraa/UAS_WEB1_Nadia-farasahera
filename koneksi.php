452d7e88a0b500bd118788bf42adbdd43750d165
nfrshraa <nfarasahera@gmail.com>
2026-01-15 09:56:06 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$username ="root";
+$password ="";
+$dbname ="db_penjualan2";
+
+$conn = new mysqli($host, $username,$password,$dbname);
+
+if($conn->connect_error){
+    die("koneksi gagal: ". $conn->connect_error);
+}
